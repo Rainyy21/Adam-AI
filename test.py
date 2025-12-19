e3baf026e75f2d6ace7ecced42614b816955d437
Rainyy21 <simonzheng950@gmail.com>
2025-12-19 16:36:53 -0500

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+BAD_WORD = [
+    "rape", "bitch",
+    "fuck", "shut up",
+    "stupid", "rude",
+    "dumb", "molest",
+    "idiot", "moron",
+    "retard", "damn",
+    "hell", "asshole",
+    "kys", "kill yourself",
+    "grape", "negative",
+    "hate"
+    ]
+
+bob = True
+if not 5>10:
+    print("True")
+else :
+    print("False")
